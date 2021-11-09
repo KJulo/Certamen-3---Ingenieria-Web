61bb45e5b6d2f08d7321fe017a2e91b3e913af18
Ignacio <kjulo.c@gmail.com>
2021-11-09 10:43:30 -0300

    Creacion del navbar
    Se crea el navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+
+  estados = [
+    { title: 'Iniciada', color: 'red'},
+    { title: 'En proceso', color: 'yellow'},
+    { title: 'Terminado', color: 'green'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
